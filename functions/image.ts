869f1549d99b9c53cfb25b1f962ee39562f83b39
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-19 10:46:13 +0100

    image

---
+++ functions/image.ts
@@ -0,0 +1,9 @@
+const url = 'https://image.shutterstock.com/image-photo/graphs-representing-stock-market-crash-600w-1658501806.jpg'
+export const onRequest: PagesFunction = async ({request}) => {
+    const accept = request.headers.get('accept')
+    const options: RequestInit | undefined = accept?.includes('image/avif')
+        ? { cf: { image: { format: 'avif' } } }
+        : undefined    
+    const img = await fetch(url, options)
+    return img;
+}
--- functions/time.ts
+++
@@ -1,1 +0,0 @@
-export const onRequest: PagesFunction = () => new Response(new Date().toISOString())
