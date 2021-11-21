eb5e1d3e624a0a854bbca099e28f27597d31923b
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-21 14:33:20 +0100

    trycatch

--- functions/image.ts
+++ functions/image.ts
@@ -1,10 +1,17 @@
+import { jsonResponse } from "./utilities"
+
 const url = 'https://image.shutterstock.com/image-photo/graphs-representing-stock-market-crash-600w-1658501806.jpg'
 export const onRequest: PagesFunction = async ({request}) => {
-    const accept = request.headers.get('accept')
-    const options: RequestInit | undefined = accept?.includes('image/avif')
-        ? { cf: { image: { format: 'avif' } } }
-        : undefined    
-    const img = await fetch(url, options)
-    img.headers.set('x-this-was-accepted', accept || 'undefined')
-    return img
+    try {
+        const accept = request.headers.get('accept')
+        const options: RequestInit | undefined = accept?.includes('image/avif')
+            ? { cf: { image: { format: 'avif' } } }
+            : undefined
+    
+        const img = await fetch(url, options)
+        img.headers.set('x-this-was-accepted', accept || 'undefined')
+        return img
+    } catch (error) {
+        return jsonResponse(error, { status: 500 })
+    }
 }
