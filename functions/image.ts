8be2e7716ba8b3599322e4f8881245f636a32e8b
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-22 01:16:40 +0100

    pielen

--- functions/image.ts
+++ functions/image.ts
@@ -1,13 +1,32 @@
 import { jsonResponse } from "./utilities"
 
-const url = 'https://image.shutterstock.com/image-photo/graphs-representing-stock-market-crash-600w-1658501806.jpg'
+const imageUrl = 'https://image.shutterstock.com/image-photo/graphs-representing-stock-market-crash-600w-1658501806.jpg'
 export const onRequest: PagesFunction = async ({request}) => {
     // const accept = request.headers.get('accept')
     // const options: RequestInit | undefined = accept?.includes('image/avif')
     //         ? undefined // ? { cf: { image: { format: 'avif' } } }
     //         : undefined
     try {
-        const img = await fetch(url, { cf: { image: { format: 'webp' } } })
+        let url = new URL(request.url)
+
+        // Cloudflare-specific options are in the cf object.
+        const options: any= { cf: { image: {} } }
+
+        // Copy parameters from query string to request options.
+        // You can implement various different parameters here.
+        if (url.searchParams.has("fit")) options.cf.image.fit = url.searchParams.get("fit")
+        if (url.searchParams.has("width")) options.cf.image.width = url.searchParams.get("width")
+        if (url.searchParams.has("height")) options.cf.image.height = url.searchParams.get("height")
+        if (url.searchParams.has("quality")) options.cf.image.quality = url.searchParams.get("quality")
+
+        // Your Worker is responsible for automatic format negotiation. Check the Accept header.
+        const accept = request.headers.get("Accept");
+        if (accept && /image\/avif/.test(accept)) {
+            options.cf.image.format = 'avif';
+        } else if (accept && /image\/webp/.test(accept)) {
+            options.cf.image.format = 'webp';
+        }
+        const img = await fetch(imageUrl, options)
         // img.headers.set('x-this-was-accepted', accept || 'undefined')
         return img
     } catch (error) {
