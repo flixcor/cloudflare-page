26041fbf2fd7df0e0a3a0737bd2a140107be0063
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-27 00:36:42 +0100

    debug

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -12,7 +12,7 @@ const preloadMarkerIndex = template.indexOf(preloadMarker)
 const start = template.substring(0, preloadMarkerIndex)
 const between = template.substring(preloadMarkerIndex + preloadMarker.length, appMarkerIndex)
 const end = template.substring(appMarkerIndex + appMarker.length)
-new HTMLRewriter().on('head', )
+
 class CommentHandler implements HTMLRewriterElementContentHandlers {
     constructor(private preloadLinks: string, private html: string){}
     comments(comment: Comment) {
@@ -52,31 +52,4 @@ async function handle(request: Request, response: Response) {
     return new HTMLRewriter()
         .on('div#app, head', handler)
         .transform(response)
-}
-
-async function render(intermediateResponse: Response, path: string) {
-    if(!intermediateResponse.headers.get('content-type')?.includes('text/html')){
-        return intermediateResponse
-    }
-
-    const [html, preloadLinks] = await createRenderer(path, manifest)
-    const all = [
-        start,
-        preloadLinks,
-        between,
-        html,
-        end
-    ]
-
-    return new Response(all.join(), intermediateResponse)
-}
-
-async function writeStrings(writable: WritableStream<string>, strings: string[]) {
-    const writer = writable.getWriter()
-    strings.forEach(async (x) => {
-        await writer.ready
-        await writer.write(x)
-    })
-    await writer.ready
-    await writer.close()
 }
