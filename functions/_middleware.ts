cffa249c93dad9c7f2f91cb78699a8d774c2cf81
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-26 20:31:32 +0100

    debug

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -20,7 +20,6 @@ async function render(intermediateResponse: Response) {
         if(!intermediateResponse.headers.get('content-type')?.includes('text/html')) return intermediateResponse
         const template = await intermediateResponse.text()
         const index = template.indexOf(htmlMarker)
-        return new Response(index.toString())
         if(index === -1) return intermediateResponse
         const before = template.substring(0, index)
         const after = template.substring(index + htmlMarker.length)
@@ -30,12 +29,6 @@ async function render(intermediateResponse: Response) {
         const writer = writable.getWriter()
         await writer.write(before.replace(`<!--preload-links-->`, preloadLinks))
         pipe(writable)
-        try {
-            await writer.ready    
-        } catch (error) {
-            console.log(error)
-        }
-        
-        await writer.write(after)
-        return new Response(readable)
+        // await writer.write(after)
+        return new Response(readable, intermediateResponse)
 }
