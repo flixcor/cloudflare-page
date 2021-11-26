839bd22b5bbf40fe6851a38311c02caa866d5bcb
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-26 13:51:03 +0100

    ssr

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,7 +1,29 @@
+import { createRenderer } from "../src/entry-server"
+import manifest from '../dist/ssr-manifest.json'
+const htmlMarker = `<!--app-html-->`
+
 export const onRequest: PagesFunction[] = [
-    async ({next, data}) => {
-        const response = await next()
-        response.headers.set('X-Hello', 'World')
-        return response
+    async ({next}) => {
+        const intermediateResponse = await next()
+        if(!intermediateResponse.headers.get('content-type')?.includes('text/html')) return intermediateResponse
+        const template = await intermediateResponse.text()
+        const index = template.indexOf(htmlMarker)
+        if(index === -1) return intermediateResponse
+        const before = template.substring(0, index)
+        const after = template.substring(index + htmlMarker.length)
+        const [pipe, preloadLinks] = await createRenderer(intermediateResponse.url, manifest)
+
+        const {readable, writable} = new TransformStream();
+        const writer = writable.getWriter()
+        await writer.write(before.replace(`<!--preload-links-->`, preloadLinks))
+        pipe(writable)
+        try {
+            await writer.ready    
+        } catch (error) {
+            console.log(error)
+        }
+        
+        await writer.write(after)
+        return new Response(readable)
     }
 ]
---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import {
+    createMemoryHistory,
+    createRouter as _createRouter,
+    createWebHistory
+} from 'vue-router'
+
+export const createRouter = () => _createRouter({
+    // use appropriate history implementation for server/client
+    // import.meta.env.SSR is injected by Vite.
+    history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
+    routes: [
+        {
+            path: '/:pathMatch(.*)',
+            component: () => import('./components/HelloWorld.vue')
+        }
+    ]
+})
