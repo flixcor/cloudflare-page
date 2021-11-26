81b42c9461493251f349635083663976852f7d17
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-26 20:41:18 +0100

    debug

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -22,13 +22,22 @@ async function render(intermediateResponse: Response) {
         const index = template.indexOf(htmlMarker)
         if(index === -1) return intermediateResponse
         const before = template.substring(0, index)
-        // const after = template.substring(index + htmlMarker.length)
+        const after = template.substring(index + htmlMarker.length)
+
+        return new Response(after)
+
         const [pipe, preloadLinks] = await createRenderer(intermediateResponse.url, manifest)
-        return new Response(preloadLinks)
+
         const {readable, writable} = new TransformStream();
         const writer = writable.getWriter()
         await writer.write(before.replace(`<!--preload-links-->`, preloadLinks))
         pipe(writable)
-        // await writer.write(after)
-        return new Response(readable, intermediateResponse)
+        try {
+            await writer.ready    
+        } catch (error) {
+            console.log(error)
+        }
+        
+        await writer.write(after)
+        return new Response(readable)
 }
