41b41fd403c32986e63f5a17d5a51ef2a46e7e89
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-27 13:40:57 +0100

    geen supsense

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,7 @@
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view v-slot="{ Component }">
-      <suspense>
-        <template #default>
-          <component :is="Component"/>
-        </template>
-        <template #fallback>
-          <div>Loading...</div>
-        </template>
-      </suspense>
-    </router-view>
+  <router-view />
 </template>
 
 <style>
