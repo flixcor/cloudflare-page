467288098ea55ab65f2f344e2bb778e707b27180
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-27 14:27:37 +0100

    nog simpeler

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <img class="dp-b" alt="Vue logo" src="./assets/logo.png" />
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <img class="dp-b" alt="Vue logo" src="./assets/logo.png" />
+    <hello-world msg="Home" />
+  </div>
 </template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
 
 <style>
 #app {
