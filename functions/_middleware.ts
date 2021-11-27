dc624e5edd49444b5d0e3bdebfa1171cbffe9f4f
Felix Cornelissen <felixcornelissen@gmail.com>
2021-11-27 02:02:41 +0100

    debug

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -2,15 +2,24 @@ import { parseURL } from "ufo"
 import { createRenderer } from "../dist/entry-server"
 import manifest from '../dist/ssr-manifest.json'
 
+const decoder = new TextDecoder()
 class CommentHandler implements HTMLRewriterDocumentContentHandlers {
     constructor(private preloadLinks: string, private html: ReadableStream){}
-    comments(comment: Comment) {
+    async comments(comment: Comment) {
         if(comment.text.includes('preload')){
             comment.replace(this.preloadLinks, {
                 html: true
             })
         } else if (comment.text.includes('html')) {
-            comment.replace(this.html)
+            comment.remove()
+            const reader = this.html.getReader()
+            while(true) {
+                const {done, value} = await reader.read()
+                if(value) {
+                    comment.after(decoder.decode(value))
+                }
+                if(done) return
+            }
         }
     }
 }
@@ -25,12 +34,7 @@ const ssr: PagesFunction = async ({request, next}) => {
         const [render, preloadLinks] = await createRenderer(pathname, manifest)
         const {readable, writable} = new TransformStream()
         render(writable)
-        try {
-            await writable.close()
-        } catch (error) {
-            console.log(error)
-        }
-        
+        return new Response(readable)
         const handler = new CommentHandler(preloadLinks, readable)
         return new HTMLRewriter()
             .onDocument(handler)
